bad90477fbfc6a15ff1bc955c50fb38639d9b035
carlacodes <carla.griffiths@gmail.com>
2019-01-29 00:28:32 +0000

    29_1_2019 from 28

---
+++ startbootstrap-business-casual-gh-pages/js/index.js
@@ -0,0 +1,19 @@
+//function to capture and store each form data into an information array
+function processForm(){
+    alert("Data has been submitted.");
+    let first_namejs = document.querySelector("#firstname").value;
+    let second_namejs = document.querySelector("#lastname").value;
+    let emailjs=document=document.querySelector("#email").value;
+    //creating an instance variable called infoarrray
+    let infoarray=[first_namejs, second_namejs,emailjs];
+
+    console.log(Object.values(infoarray));
+}
+
+//alert("hello");
+function Regurgitate(){
+
+    let text=document.querySelector("#srch-term").value;
+    alert("You searched for " + text);
+}
+
