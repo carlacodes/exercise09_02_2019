c13aaa4d4dd13e557ed9f0bb4776b082218c3e92
carlacodes <carla.griffiths@gmail.com>
2019-02-05 15:55:58 +0000

    3:55PM 05_02_2019

--- startbootstrap-business-casual-gh-pages/dataphp/searchSakila.php
+++ startbootstrap-business-casual-gh-pages/dataphp/searchSakila.php
@@ -1,6 +1,6 @@
 <?php
 
-
+function findTerm(){
 require_once('databasephp.php');
     //$srch_term = $_GET['srch-term'] ?? '1'; //PHP 7.0
     $srch_term = $_POST['srch-term'] ?? '1'; //dataphp 7.0
@@ -8,34 +8,24 @@ require_once('databasephp.php');
     $query .= "WHERE title = '" . $srch_term."'";
 
     $connection = connectToDb();
-
-    $result = msysqli_query($connection, $query);
-
+//search database
+    $result = mysqli_query($connection, $query);
     if (!$result){
         exit("databasePhp query failed.");
     }
 
     $user = mysqli_fetch_assoc($result);
-    ?>
-    <table class="table">
-        <tr>
-            <th>titles</th>
 
-        </tr>
 
-        <?php while ($user = mysqli_fetch_array($result)) { ?>
-            <tr>
-                <td><?php echo $user['title'] ?></td>
-            </tr>
-        <?php } ?>
-    </table>
-
-    <?php
     // Free the results from memory
     mysqli_free_result($result);
 
     // Close the connection
-    closeDb($connection);
+    mysqli_close($connection);}
 
 
-?>
+
+
+
+
+
---
+++ startbootstrap-business-casual-gh-pages/dataphp/searchSakila1.php
@@ -0,0 +1,11 @@
+
+ <table class="table" action=<? findTerm()?> >
+        <tr>
+            <th>titles</th>
+
+        </tr>
+            <tr> findTerm();
+                <td><?php echo $user['title'] ?></td>
+            </tr>
+
+    </table>
